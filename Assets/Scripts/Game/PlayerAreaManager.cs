03e94c17fb955e0cd5cd14b5103eeeb2be174657
shen <w820912@gmail.com>
2020-02-01 10:40:41 +0800

    [Feature] 玩家移動測試、新增控制玩家移動區域script

---
+++ Assets/Scripts/Game/PlayerAreaManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+    
+
+// [System.Serializable]
+// public class PlaygroundSettings
+// {
+//     public float width;
+//     public float height;
+//     public int[] notes;
+//     public GameObject background;
+// }
+public class PlayerManager : MonoBehaviour
+{
+    public float width;
+    public float height;
+    public int[] notes;
+    public GameObject background1;
+    public GameObject background2;
+
+    // PlaygroundSettings m_PlayergronudSettings = new PlaygroundSettings();
+    // Start is called before the first frame update
+    void Start()
+    {
+        RectTransform rt = background1.GetComponent<RectTransform>();
+        RectTransform rt2 = background2.GetComponent<RectTransform>();
+        rt.sizeDelta = new Vector2 (width, height);
+        rt2.sizeDelta = new Vector2 (width, height);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
