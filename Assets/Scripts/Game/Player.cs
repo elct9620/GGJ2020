fd91081083deb3eebed35e0b3ac59059e5b2be62
shen <w820912@gmail.com>
2020-02-01 12:24:32 +0800

    [Feature] Player Smooth移動、新增Player1, 2分別操控

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -6,12 +6,13 @@ public class Player : MonoBehaviour
 {
     public float movingTime = 0.1f;
     //public LayerMask blockingLayer;            //Layer on which collision will be checked.
-
+    public int step = 10;
+    
     public GameObject movingArea;
     private BoxCollider2D boxCollider;         //The BoxCollider2D component attached to this object.
     private Rigidbody2D rb2D;                //The Rigidbody2D component attached to this object.
     private float inverseMoveTime;            //Used to make movement more efficient.
-
+    private Vector2 endPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,11 +22,20 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // int horizontal;
         int vertical = 0;
-        vertical = (int) (Input.GetAxisRaw ("Vertical"));
+        switch (gameObject.name)
+        {
+            case ("Player1"):
+                vertical = (int) (Input.GetAxisRaw ("Player1Vertical"));
+                break;
+            case ("Player2"):
+                vertical = (int) (Input.GetAxisRaw ("Player2Vertical"));
+                break;
+        }
+        
 
         if (vertical != 0)
         {
@@ -35,29 +45,31 @@ public class Player : MonoBehaviour
     }
     void tryMove(int vertical)
     {
-        // mid = 379
         Vector2 nowPosition = transform.position;
-        Vector2 endPosition = nowPosition + new Vector2 (0, vertical);
+        endPosition = nowPosition + new Vector2 (0, 10 * vertical);
         RectTransform rt = movingArea.GetComponent<RectTransform>();
         float gap = 20;
-        float topMost = 379 + rt.rect.height * 0.5f  - gap;
-        float bottomMost = 379 - rt.rect.height * 0.5f  + gap;
+        float topMost =  rt.rect.height * 0.5f  - gap;
+        float bottomMost = -1 * rt.rect.height * 0.5f  + gap;
         if (endPosition.y > bottomMost && endPosition.y < topMost)
+        {
+            StopAllCoroutines();
             StartCoroutine(SmoothMovement(endPosition));
+
+        }
+            // transform.position = endPosition;
     }
     protected IEnumerator SmoothMovement (Vector3 end)
     {
         //Calculate the remaining distance to move based on the square magnitude of the difference between current position and end parameter. 
         //Square magnitude is used instead of magnitude because it's computationally cheaper.
         float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
-        Debug.LogError("in sqrRemainingDistance!" + sqrRemainingDistance);
         //While that distance is greater than a very small amount (Epsilon, almost zero):
         while(sqrRemainingDistance > float.Epsilon)
         {   
-            Debug.LogError("in float.Epsilon!" + float.Epsilon);
             //Find a new position proportionally closer to the end, based on the moveTime
-            Vector3 newPostion = Vector3.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
-
+            Vector3 newPostion = Vector3.MoveTowards(rb2D.position, end, step * inverseMoveTime * Time.deltaTime);
+            Debug.LogError("in newPostion!" + newPostion);
             //Call MovePosition on attached Rigidbody2D and move it to the calculated position.
             rb2D.MovePosition (newPostion);
 
