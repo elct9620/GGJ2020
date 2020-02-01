48affa26031b817f98c6456e535fc89882be8289
蒼時弦也 <elct9620@frost.tw>
2020-02-01 21:27:01 +0800

    Add track bottom fire effect

--- Assets/Scripts/Game/Track.cs
+++ Assets/Scripts/Game/Track.cs
@@ -13,6 +13,10 @@ public class Track : MonoBehaviour
     public float SquarePitchPercentage = 0.33f;
     public float TrianglePitchPercentage = 0.66f;
     public float FilledPercentage = 0.5f;
+    public SpriteRenderer BottomFire;
+    public int PreferCombo = 10;
+
+    public int Combo = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +26,7 @@ public class Track : MonoBehaviour
 
         TimeOffset = Camera.main.orthographicSize * 2;
 
+        UpdateBottomAlpha();
         StartCoroutine(CreateNote());
     }
 
@@ -38,8 +43,8 @@ public class Track : MonoBehaviour
             if (maxPitch < note.Pitch) { maxPitch = note.Pitch; }
             if (minPitch > note.Pitch) { minPitch = note.Pitch; }
         }
-        int squarePitch = (int) Mathf.Lerp((float) minPitch, (float) maxPitch, SquarePitchPercentage);
-        int trianglePitch = (int) Mathf.Lerp((float) minPitch, (float) maxPitch, TrianglePitchPercentage);
+        int squarePitch = (int)Mathf.Lerp((float)minPitch, (float)maxPitch, SquarePitchPercentage);
+        int trianglePitch = (int)Mathf.Lerp((float)minPitch, (float)maxPitch, TrianglePitchPercentage);
 
         foreach (NoteData note in Score.Notes)
         {
@@ -50,19 +55,45 @@ public class Track : MonoBehaviour
 
             Slot slot = Instantiate(SlotPrefab, Slots.transform);
             slot.Note = note;
-            if (note.Pitch > trianglePitch) {
+            if (note.Pitch > trianglePitch)
+            {
                 slot.SetType(Slot.Types.Triangle);
-            } else if (note.Pitch > squarePitch) {
+            }
+            else if (note.Pitch > squarePitch)
+            {
                 slot.SetType(Slot.Types.Square);
-            } else {
+            }
+            else
+            {
                 slot.SetType(Slot.Types.Circle);
             }
             slot.Source.clip = Score.Clip;
 
-            if (Random.value < FilledPercentage) {
+            if (Random.value < FilledPercentage)
+            {
                 slot.Filled = true;
             }
 
+            slot.OnExit += OnSlotExit;
         }
+    }
+
+    public void OnSlotExit(bool Filled)
+    {
+        if (!Filled)
+        {
+            Combo = 0;
+            UpdateBottomAlpha();
+            return;
+        }
+
+        Combo += 1;
+        UpdateBottomAlpha();
+    }
+
+    private void UpdateBottomAlpha()
+    {
+        float alpha = Mathf.Lerp(0.25f, 1.0f, (Combo * 1.0f) / (PreferCombo * 1.0f));
+        BottomFire.color = new Color(BottomFire.color.r, BottomFire.color.g, BottomFire.color.b, alpha);
     }
 }
