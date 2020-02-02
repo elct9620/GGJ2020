d3b84c686ae157628bb0c2943a4ae84bfccc0cb3
蒼時弦也 <elct9620@frost.tw>
2020-02-02 13:51:28 +0800

    Add opening scene with type effect

---
+++ Assets/Scripts/Scene/Opening.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Opening : MonoBehaviour
+{
+    public Text Message;
+    public float typeSpeed = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Typing());
+    }
+
+    public IEnumerator Typing()
+    {
+        char[] characters = Message.text.ToCharArray();
+        Message.text = "";
+        yield return new WaitForSeconds(0.5f);
+
+        foreach (char word in characters)
+        {
+            Message.text += word;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
