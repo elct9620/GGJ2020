44915fc9b3b2508ec0b300770a7de6ab09e84524
shen <w820912@gmail.com>
2020-02-02 10:24:29 +0800

    [Feature] 新增Main scene, 切換Scene功能(回首頁、進入遊戲), Replay功能, 調整track深度為default

--- Assets/Scripts/Scene/Gameplay.cs
+++ Assets/Scripts/Scene/Gameplay.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using UnityEngine.SceneManagement;
 public class Gameplay : MonoBehaviour
 {
     public static Text scoreText;
     public Text Time;
     public Text Name;
     public GameObject GO;
+    public GameObject ResultPanel;
+    public GameObject Player1;
+    public GameObject Player2;
     public static int score;
     // Start is called before the first frame update
     void Start()
@@ -27,10 +31,25 @@ public class Gameplay : MonoBehaviour
         string seconds = Convert.ToString(Convert.ToInt32(Mathf.Floor(playbackTime % 60)));
 
         Time.text = String.Format("{0}:{1}", minutes.PadLeft(2, '0'), seconds.PadLeft(2, '0'));
+        if (minutes == "0" && seconds == "0")
+            ResultPanel.SetActive(true);
+            ResultPanel.GetComponent<Animator>().Play("Popup");
     }
     public static void UpdateScore()
     {
         Gameplay.score += 10;
         Gameplay.scoreText.text = "Score : " + Gameplay.score.ToString();
+    }
+    public void RestartGame()
+    {
+        // 重新開始
+        MusicPlayer.main.Replay();
+        ResultPanel.SetActive(false);
+        Player1.transform.localPosition = new Vector3 (-4, 0, 0);
+        Player2.transform.localPosition = new Vector3 (4, 0, 0);
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Main");
     }
 }
